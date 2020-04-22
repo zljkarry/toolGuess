7124c72c65978adc4aab5a6c8540291161bcf2ba
zljkarry <zhaolijia@redrock.team>
2020-04-22 15:22:53 +0800

    闯关成功页UI

--- src/views/over.vue
+++ src/views/over.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-    <div class="title">闯关成功</div>
-    <div class="img"></div>
-    <div class="time">用时：{{$store.state.time}}s</div>
-    <div class="rank">排名：{{$store.state.rank}}</div>
-    <div class="hint" v-show="!this.$store.state.valid">由于您在该难度已经挑战过3次，该次记录将不会记录于排行榜中</div>
-    <div class="btn" @click="goHome">返回首页</div>
+  <div class="success">
+    <background-img></background-img>
+    <div class="title"></div>
+    <div class="time_bar">
+      <img class="icon_time" src="../assets/img/icon/time.png" alt />
+
+      <div class="time">{{$store.state.time}} s</div>
+    </div>
+    <div class="ranking_bar">
+      <img class="icon_ranking" src="../assets/img/icon/ranking.png" alt />
+      <div class="ranking">
+        {{$store.state.rank}}
+        <span>th</span>
+      </div>
+    </div>
+    <img src="../assets/img/go_home.png" class="btn" @click="goHome" />
+    <div class="hint">
+       <!-- v-show="!this.$store.state.valid" -->
+      <p>由于您在该难度已经挑战过3次</p>
+      <p>该次记录将不会记录于排行榜中</p>
+    </div>
   </div>
 </template>
 
 <script>
-import backgroundImg from '@/components/backgroundimg.vue'
+import backgroundImg from "@/components/backgroundimg.vue";
 export default {
   data() {
     return {
@@ -29,4 +43,63 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.success {
+  overflow: hidden;
+  .title {
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    position: absolute;
+    background-image: url("../assets/img/success.png");
+    background-repeat: no-repeat;
+    background-size: 100%;
+  }
+  .time_bar {
+    height: 56px;
+    margin: 662px auto auto 276px;
+    display: flex;
+    .icon_time {
+      width: 55px;
+      height: 56px;
+      margin-right: 42px;
+    }
+    .time {
+      font-family: "fangZheng";
+      font-size: 44px;
+      line-height: 56px;
+      color: #973333;
+    }
+  }
+  .ranking_bar {
+    height: 68px;
+    margin: 10px auto auto 276px;
+    display: flex;
+    .icon_ranking {
+      width: 47px;
+      height: 68px;
+      margin-right: 50px;
+    }
+    .ranking {
+      font-family: "fangZheng";
+      font-size: 44px;
+      line-height: 68px;
+      color: #973333;
+      span {
+        font-size: 30px;
+      }
+    }
+  }
+  .btn {
+    width: 252px;
+    height: 98px;
+    margin: 40px 0 42px 245px;
+  }
+  .hint {
+    text-align: center;
+    font-family: "yueHei";
+    font-size: 24px;
+    color: #7f2727;
+    line-height: 34px;
+  }
+}
 </style>
